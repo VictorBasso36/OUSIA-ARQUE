b860cf63d68f10b948c3876442ef0ba2ebb9e39c
Victor Basso <66968560+VictorBasso36@users.noreply.github.com>
2022-03-28 14:36:23 -0300

    Add files via upload

--- src/js/leiamaisleiamenos.js
+++ src/js/leiamaisleiamenos.js
@@ -7,14 +7,14 @@ var textepage2 = document.querySelector('.textepage2')
 var x = window.screen.width;
 var y = window.screen.height;
     
-    if(x >520){
+    if(x >520 && y > 600){
         pontos.style.display='none';
         mais.style.display = 'none';
         btnleiamais.style.display = 'none';
         trocaptext1.textContent="Acreditamos que todas as pessoas tem um grande potencial, e é assim que nosso grupo vem crescendo: Dando espaço e oportunidades para que nossa equipe troque vivências, experiências e aprendizados. Somos formados por pessoas que amam o que fazem e acreditam que podem mudar o mundo através de seus conhecimentos."
         trocaptext2.textContent="O Grupo Ousía Arqué se empenha a diariamente desenvolver ideias e marcas para serem REVOLUÇÃO!";
     } else{
-        if(x <520){
+        if(x <520 && y < 600){
             
         }
     }
